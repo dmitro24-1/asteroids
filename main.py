7506a35dbb1e356384c4d847373787e6aa275cef
dmitro <xardbacc56gzh@gmail.com>
2025-03-02 11:02:02 +0100

    turning right and left

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    dt = 0
+    time = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
-    
+        dt = time.tick(60) / 1000
+        
 
 
 
